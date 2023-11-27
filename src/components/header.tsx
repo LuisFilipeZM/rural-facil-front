196fb92d3cac2aa8e6825862b3105add9638adbe
Luis Filipe <luis_filipezm@hotmail.com>
2023-11-26 23:17:15 -0300

    novos cadastros

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,31 @@
 import { Link } from 'react-router-dom';
 import logoHeader from '../assets/logo-header.svg';
 import { FaSignOutAlt } from 'react-icons/fa';
-
+import { useEffect, useState } from 'react';
 
 export function Header() {
     const user = JSON.parse(localStorage.getItem('user') || '{}');
+    const [isAdmin, setIsAdmin] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`http://localhost:8080/api/acessopessoa/${user.id}`, {
+                    headers: {
+                        Authorization: `Bearer ${user.accessToken}`
+                    }
+                });
+                const data = await response.json();
+                if (data[0].admin === "ROLE_ADMIN") {
+                    setIsAdmin(true);
+                }
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
 
     function logout() {
         localStorage.removeItem('user');
@@ -24,12 +45,15 @@ export function Header() {
                 </div>
                 <div>
                     {user && Object.keys(user).length !== 0 ? (
-                        <p>
-                            {user.username} 
-                            <a href="/" onClick={logout} style={{color: 'red', textDecoration: 'none'}}>
-                                <FaSignOutAlt style={{verticalAlign: 'middle', marginLeft: '0.5rem'}} />
+                        <>{isAdmin && (
+                            <Link to="/cadastro-agricultor" className="btn btn-success btn-sm m-2">Cadastrar agricultor</Link>
+                        )}
+                            <Link to="/dados-cliente" style={{ textDecoration: 'none', color: "black" }}>{user.username}</Link>
+                            <a href="/" onClick={logout} style={{ color: 'red', textDecoration: 'none' }}>
+                                <FaSignOutAlt style={{ verticalAlign: 'middle', marginLeft: '0.5rem' }} />
                             </a>
-                        </p>
+                        </>
+
                     ) : (
                         <>
                             <Link to="/cadastro-cliente" className="btn btn-outline-success m-2">Cadastre-se</Link>
